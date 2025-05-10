15af461b3284eb670daaccf04c76447b3cf26cf5
tdragojlovic1723it <tdragojlovic1723it@raf.rs>
2025-05-11 00:18:39 +0200

    dodate migracije, seederi (ne skroz), modeli i kontroler za sorte

---
+++ database/migrations/2025_05_10_220725_create_sortas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sortas', function (Blueprint $table) {
+            $table->id();
+            $table->string("kind");
+            $table->text("description");
+            $table->decimal("average_fruit_size", 8, 2);
+            $table->decimal("average_fertility", 8, 1);
+            // TODO: napravi da ovde ima default slika
+            $table->string("image");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sortas');
+    }
+};
---
+++ database/seeders/SortaSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sorta;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SortaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Sorta::create([
+            'kind' => 'Bluecrop',
+            'description' => '...',
+            'average_fruit_size' => 1.8,
+            'average_fertility' => 8,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Duke',
+            'description' => '...',
+            'average_fruit_size' => 1.75,
+            'average_fertility' => 7,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Legacy',
+            'description' => '...',
+            'average_fruit_size' => 2,
+            'average_fertility' => 10,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Chandler',
+            'description' => '...',
+            'average_fruit_size' => 2.5,
+            'average_fertility' => 6,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Elliott',
+            'description' => '...',
+            'average_fruit_size' => 1.6,
+            'average_fertility' => 7.5,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Brigitta',
+            'description' => '...',
+            'average_fruit_size' => 2.05,
+            'average_fertility' => 1.4,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Sunshine Blue',
+            'description' => '...',
+            'average_fruit_size' => 1.4,
+            'average_fertility' => 5,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Top Hat',
+            'description' => '...',
+            'average_fruit_size' => 1.25,
+            'average_fertility' => 4,
+            'image' => 'default.jpg',
+        ]);
+
+        Sorta::create([
+            'kind' => 'Aurora',
+            'description' => '...',
+            'average_fruit_size' => 2,
+            'average_fertility' => 8,
+            'image' => 'default.jpg',
+        ]);
+    }
+}
